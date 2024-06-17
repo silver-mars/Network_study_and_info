710cff7756bcd7bcacd22d6370a5f5172ab53bf8
silver-mars <Lebortello@yandex.ru>
2024-06-17 19:27:00 +0300

    fix

--- bash/uz_login.sh
+++ bash/uz_login.sh
@@ -7,7 +7,7 @@ namespace=''
 pass=''
 
 curl -s --request POST \
-  --url https://"$secman_url"/v1/auth/ad/"$path_to_secret"/login/"$login" \
+  --url https://"$secman_url"/v1/auth/"$path_to_secret"/login/"$login" \
   --header 'Content-Type: application/json' \
   --header "X-Vault-Namespace: "$namespace"" \
   --data "{\"password\": \""${pass}"\"}" | jq
